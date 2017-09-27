e3ed6162e6672578696cb624300842b944fe15d5
Xiaochen Du <duxiaochen@yahoo.com>
2017-09-26 22:25:17 -0400

    little bee

--- web/p5/examples/sketch.js
+++ web/p5/examples/sketch.js
@@ -1,13 +1,69 @@
+var x = window.innerWidth/2;
+var y = window.innerHeight/2;
+var radius = 12;
+
 function setup() {
   createCanvas(window.innerWidth, window.innerHeight);
-
+  ellipseMode(RADIUS);
+  textSize(64);
+  background( 200);
 }
 
 function draw() {
-  if (mouseIsPressed) {
-    fill(255,0,0);
+  translate(mouseX, mouseY);
+  scale(mouseX / 60.0);
+  rect(-15, -15, 30, 30);
+}
+
+function draw6() {
+  translate(mouseX, mouseY);
+  rotate(mouseX / 200.0);
+  rect(0, 0, 160, 20);
+}
+
+function draw5() {
+  translate(mouseX, mouseY);
+  rect(0, 0, 30, 30);
+  translate(35, 10);
+  rect(0, 0, 15, 15);
+}
+
+function draw4() {
+  translate(mouseX, mouseY);
+  rect(0, 0, 30, 30);
+}
+
+function draw3() {
+  background(204);
+  stroke(255);
+  line(x, y, mouseX, mouseY); // White line
+  stroke(0);
+  var mx = map(mouseX, 0, width, 60, 180);
+  line(x, y, mx, mouseY); // Black line
+}
+
+function draw2() {
+  if (keyIsPressed) {
+    if (keyCode == LEFT_ARROW) {            // If it's the left arrow
+      x-=10;
+    }
+    else if (keyCode == RIGHT_ARROW) {      // If it's the right arrow
+      x+=10;
+    }
+  }
+
+  background( 200);
+  text( key, x, y);
+}
+
+function draw1() {
+  background(204);
+  var d = dist(mouseX, mouseY, x, y);
+  if (d < radius) {
+    radius++;
+    fill(0);
   } else {
     fill(255);
   }
-  ellipse(mouseX, mouseY, 80, 80);
+  ellipse(x, y, radius, radius);
 }
--- web/sketch.js
+++ web/sketch.js
@@ -1,9 +1,30 @@
+var img;
+
+function preload() {
+  img = loadImage('bee.png');
+}
+
 function setup() {
-  createCanvas(1024, 768);
+  var cnv = createCanvas(windowWidth, windowHeight);
+  var x = (windowWidth - width) / 2;
+  var y = (windowHeight - height) / 2;
+  cnv.position(x, y);
+  //background(255, 0, 200);
+  frameRate(5);
+
+}
+var pre_x = 100;
+var pre_y = 100;
+function draw() {
+  clear();
+//   wait(1000);
+  pre_x = pre_x + random(-5, 20);
+  pre_y = pre_y + random(-10,10);
+  image(img,pre_x, pre_y);
 
 }
 
-function draw() {
+function draw2() {
   if (mouseIsPressed) {
     fill(255,0,0);
   } else {
